764074f50306e3f1795788c8b27f19e12eb8a0f5
mikejway <110037320+mikejway@users.noreply.github.com>
2022-07-26 14:07:26 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 setup(
     name='gb_api_tools',
     version='0.0.1',
-    python_requires='>3.6.13',
+    python_requires='>=3.6',
     author_email='michael.way@generalbioinformatics.com',
-    license_files = ('LICENSE.txt',),
+    license_files = ('LICENSE.txt'),
     install_requires=[
-        'pandas'==1.1.5,
-        'requests'==2.26.0,
-        'openpyxl'==3.0.0,
-        'biopython'==1.74
+        'pandas==1.1.5',
+        'requests==2.26.0',
+        'openpyxl==3.0.0',
+        'biopython==1.74'
         ],
     include_package_data=True
     )
